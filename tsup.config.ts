ab2753f17b606ef488c650af94fad90c9bdabc19
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-10-28 23:03:06 +0100

    use bun instead of tsup

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  clean: true,
-  entry: ['./src/index.ts'],
-  outDir: "dist",
-  external: ['readline'],
-  format: ['esm',],
-});
