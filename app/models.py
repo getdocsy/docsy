6dfb30f6583de8a8aa7b95d5e1b1797a5d3e422f
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-12 14:31:31 +0100

    introduce analysis to model

--- app/models.py
+++ app/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 class Repo(models.Model):
-    github_full_name = models.CharField(max_length=255)
+    nfkc_github_full_name = models.CharField(max_length=255)
+    nfkc_clone_url = models.CharField(max_length=255)
+    is_public = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.github_full_name
+        return self.nfkc_github_full_name
 
+
+class Analysis(models.Model):
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    analysis_result = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Analysis of {self.repo.nfkc_github_full_name}"
