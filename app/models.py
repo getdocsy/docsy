79cb66feb5188e91e23a3cd03d7ea9d1e9fbca01
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-12 11:30:30 +0100

    repo model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Repo(models.Model):
+    github_full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.github_full_name
+
