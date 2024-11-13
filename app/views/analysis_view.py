5fb1066c72b640c1a9af136ea08623088d60f465
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-13 13:44:31 +0100

    rudimentary html view for creating an analysis

--- app/views/analysis_view.py
+++ app/views/analysis_view.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
 
 from app import custom_errors, sanitization_utils
 from app.models import Analysis, Repo
@@ -28,12 +30,12 @@ class AnalysisViewSet(viewsets.ModelViewSet):
         return [permission() for permission in permission_classes]
 
     def create(self, request):
-        github_full_name = request.data.get('github_full_name')
-        if not github_full_name:
+        unsafe_github_full_name = request.data.get('github_full_name')
+        if not unsafe_github_full_name:
             return HttpResponse("Missing required parameter: github_full_name", status=400)
 
         sanitized_github_full_name = sanitization_utils.strip_xss(
-            unsafe_github_full_name=github_full_name
+            unsafe_github_full_name=unsafe_github_full_name
         )
 
         try:
@@ -43,3 +45,26 @@ class AnalysisViewSet(viewsets.ModelViewSet):
             return HttpResponse(analysis_result)
         except custom_errors.PublicGithubRepoNotFoundError as e:
             return HttpResponse(str(e), status=404)
+
+class AnalysisFormView(View):
+    template_name = 'analysis/create.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        github_full_name = request.POST.get('github_full_name')
+        if not github_full_name:
+            return render(request, self.template_name, {'error': 'GitHub repository name is required'})
+
+        sanitized_github_full_name = sanitization_utils.strip_xss(
+            unsafe_github_full_name=github_full_name
+        )
+
+        try:
+            analysis_result = analysis_service.analyze_remote_repo(
+                sanitized_github_full_name=sanitized_github_full_name
+            )
+            return render(request, self.template_name, {'result': analysis_result})
+        except custom_errors.PublicGithubRepoNotFoundError as e:
+            return render(request, self.template_name, {'error': str(e)})
--- docsy/urls.py
+++ docsy/urls.py
@@ -8,7 +8,7 @@ router.register(r"analysis", analysis_view.AnalysisViewSet)
 router.register(r"repo", repo_view.RepoViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("analysis/", analysis_view.AnalysisFormView.as_view(), name="analysis_form"),
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
-
