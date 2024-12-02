d23e3cdefbe96c9e1bd7447d8ae658d323e97733
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-12-02 19:18:31 +0000

    github pat for repo lookup

--- backend/src/app/services/github_auth_service.py
+++ backend/src/app/services/github_auth_service.py
@@ -1,6 +1,11 @@
 from github import Auth, Github, GithubIntegration
 from django.conf import settings
 
+# We use a PAT here. Not ideal, but we want to lookup repos without signing in as any installation and GitHub Apps can't do that.
+def get_github_client_for_pat() -> Github:
+    token = str(settings.GITHUB_REPO_LOOKUP_PAT)
+    return Github(auth=Auth.Token(token))
+
 def get_github_client(*, app_installation_id: int) -> Github:
     appAuth = Auth.AppAuth(
         settings.GITHUB_APP_ID, settings.GITHUB_APP_PRIVATE_KEY.replace("\\n", "\n")
