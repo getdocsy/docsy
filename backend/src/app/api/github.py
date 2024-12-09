19ef41b40df972e0a10c6d17ea458e096ff8c0ca
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-12-09 16:02:16 +0100

    docsy should not talk to itself

--- backend/src/app/api/github.py
+++ backend/src/app/api/github.py
@@ -3,6 +3,9 @@ from ninja import NinjaAPI, Schema
 from django.conf import settings
 import hmac
 import hashlib
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create a separate router for GitHub webhooks
 github_api = NinjaAPI(title="GitHub Webhooks", version="1.0.0", urls_namespace="github")
@@ -38,14 +41,23 @@ async def github_webhook(request, payload: PRPayload):
         sanitized_github_full_name=payload.repository["full_name"]
     )
     if not repo.enable_pull_request_analysis:
+        logger.info(
+            f"Event ignored because pull request analysis is disabled for repo: {payload.repository['full_name']}, pr: {payload.pull_request['number']}"
+        )
         return {"message": "Event ignored because pull request analysis is disabled"}
 
     # We only analyze opened PRs
     if payload.action not in ["opened"]:
+        logger.info(
+            f"Event ignored based on action: {payload.action}; repo: {payload.repository['full_name']}, pr: {payload.pull_request['number']}"
+        )
         return {"message": "Event ignored"}
 
     # Ignore PRs opened by the GitHub app itself
     if payload.pull_request["user"]["type"] == "Bot":
+        logger.info(
+            f"Event ignored because it was opened by a bot; repo: {payload.repository['full_name']}, pr: {payload.pull_request['number']}"
+        )
         return {"message": "Event ignored because it was opened by a bot"}
 
     # Analyze changes
