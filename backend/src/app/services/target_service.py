81d0277735e3b5c98d2ac3276f016d4cd808763d
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-12-19 09:10:34 +0100

    delete targets

--- backend/src/app/services/target_service.py
+++ backend/src/app/services/target_service.py
@@ -6,9 +6,12 @@ def create_target(*, description: str) -> Target:
 
 
 def get_all_targets() -> list[Target]:
-    return list(Target.objects.all().order_by('-created_at'))
+    return list(Target.objects.all().order_by("-created_at"))
 
 
 def get_target_by_id(target_id: int) -> Target:
     return Target.objects.get(id=target_id)
 
+
+def delete_target(target: Target) -> None:
+    target.delete()
--- backend/src/app/views/target_details_view.py
+++ backend/src/app/views/target_details_view.py
@@ -1,7 +1,8 @@
 from django.views.generic import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 from app.models import Target
 from app.services import target_service
 
@@ -21,3 +22,8 @@ class TargetDetailsView(View):
                 "user": request.user,
             },
         )
+
+    def post(self, request, target_id):
+        target = target_service.get_target_by_id(target_id)
+        target_service.delete_target(target)
+        return redirect(reverse('targets'))
